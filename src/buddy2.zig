d50a9745e00e58d6877987a08eaa028fb3fd6f06
Shaolong Chen <shaolong.chen@outlook.it>
2023-10-17 15:21:24 +0800

    feat: detect leaks and verbose log

--- src/buddy2.zig
+++ src/buddy2.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const log = std.log.scoped(.buddy2);
 const assert = std.debug.assert;
 const math = std.math;
 const mem = std.mem;
@@ -6,83 +8,125 @@ const isPowerOfTwo = math.isPowerOfTwo;
 const testing = std.testing;
 const Allocator = mem.Allocator;
 
-pub const Buddy2Allocator = struct {
-    const Self = @This();
-
-    manager: *Buddy2,
-    heap: []u8,
-
-    pub fn init(heap: []u8) Self {
-        var ctx_len = heap.len / 3 * 2;
-        if (!isPowerOfTwo(ctx_len)) {
-            ctx_len = fixLen(ctx_len) >> 1;
-        }
-        return Self{
-            .manager = Buddy2.init(heap[0..ctx_len]),
-            .heap = heap[ctx_len..],
-        };
-    }
-
-    pub fn allocator(self: *Self) Allocator {
-        return .{
-            .ptr = self,
-            .vtable = &.{
-                .alloc = alloc,
-                .resize = resize,
-                .free = free,
-            },
-        };
-    }
-
-    fn alloc(ctx: *anyopaque, len: usize, log2_ptr_align: u8, ret_addr: usize) ?[*]u8 {
-        _ = ret_addr;
-        const self: *Self = @ptrCast(@alignCast(ctx));
-        return self.alignedAlloc(len, log2_ptr_align);
-    }
-
-    fn alignedAlloc(self: *Self, len: usize, log2_ptr_align: u8) ?[*]u8 {
-        const alignment = @as(usize, 1) << @as(Allocator.Log2Align, @intCast(log2_ptr_align));
-
-        var unaligned_ptr = @as([*]u8, @ptrCast(self.unalignedAlloc(len + alignment - 1) orelse return null));
-        const unaligned_addr = @intFromPtr(unaligned_ptr);
-        const aligned_addr = mem.alignForward(usize, unaligned_addr, alignment);
-
-        return unaligned_ptr + (aligned_addr - unaligned_addr);
-    }
-
-    fn unalignedAlloc(self: *Self, len: usize) ?[*]u8 {
-        const offset = self.manager.alloc(len) orelse return null;
-        return @ptrFromInt(@intFromPtr(self.heap.ptr) + offset);
-    }
-
-    fn resize(ctx: *anyopaque, buf: []u8, log2_old_align: u8, new_len: usize, ret_addr: usize) bool {
-        _ = log2_old_align;
-        _ = ret_addr;
-        const self: *Self = @ptrCast(@alignCast(ctx));
-        return new_len <= self.alignedAllocSize(buf.ptr);
-    }
-
-    fn alignedAllocSize(self: *Self, ptr: [*]u8) usize {
-        const aligned_offset = @intFromPtr(ptr) - @intFromPtr(self.heap.ptr);
-        const index = self.manager.backward(aligned_offset);
-
-        const unaligned_offset = self.manager.indexToOffset(index);
-        const unaligned_size = self.manager.indexToSize(index);
-
-        return unaligned_size - (aligned_offset - unaligned_offset);
-    }
-
-    fn free(ctx: *anyopaque, buf: []u8, log2_old_align: u8, ret_addr: usize) void {
-        _ = log2_old_align;
-        _ = ret_addr;
-        const self: *Self = @ptrCast(@alignCast(ctx));
-        self.alignedFree(buf.ptr);
-    }
-
-    fn alignedFree(self: *Self, ptr: [*]u8) void {
-        self.manager.free(@intFromPtr(ptr) - @intFromPtr(self.heap.ptr));
-    }
+pub const Config = struct {
+    /// Enables emitting info messages with the size and address of every allocation.
+    verbose_log: bool = false,
 };
+
+pub const Check = std.heap.Check;
+
+pub fn Buddy2Allocator(comptime config: Config) type {
+    return struct {
+        const Self = @This();
+
+        manager: *Buddy2,
+        heap: []u8,
+
+        pub fn init(heap: []u8) Self {
+            var ctx_len = heap.len / 3 * 2;
+            if (!isPowerOfTwo(ctx_len)) {
+                ctx_len = fixLen(ctx_len) >> 1;
+            }
+            return Self{
+                .manager = Buddy2.init(heap[0..ctx_len]),
+                .heap = heap[ctx_len..],
+            };
+        }
+
+        pub fn detectLeaks(self: *const Self) bool {
+            const slice: []u8 = @as([*]u8, @ptrCast(&self.manager._longest))[0 .. self.manager.len * 2 - 1];
+            var leaks = false;
+            for (slice, 0..) |longest, i| {
+                if (longest == 0) {
+                    if (!builtin.is_test) {
+                        log.err("memory address 0x{x} leaked", .{@intFromPtr(self.heap.ptr) + self.manager.indexToOffset(i)});
+                    }
+                    leaks = true;
+                }
+            }
+            return leaks;
+        }
+
+        pub fn deinit(self: *Self) Check {
+            const leaks = self.detectLeaks();
+            self.* = undefined;
+            return @as(Check, @enumFromInt(@intFromBool(leaks)));
+        }
+
+        pub fn allocator(self: *Self) Allocator {
+            return .{
+                .ptr = self,
+                .vtable = &.{
+                    .alloc = alloc,
+                    .resize = resize,
+                    .free = free,
+                },
+            };
+        }
+
+        fn alloc(ctx: *anyopaque, len: usize, log2_ptr_align: u8, ret_addr: usize) ?[*]u8 {
+            _ = ret_addr;
+            const self: *Self = @ptrCast(@alignCast(ctx));
+            const ptr = self.alignedAlloc(len, log2_ptr_align);
+            if (config.verbose_log and ptr != null) {
+                log.info("alloc {d} bytes at {*}", .{ len, ptr.? });
+            }
+            return ptr;
+        }
+
+        fn alignedAlloc(self: *Self, len: usize, log2_ptr_align: u8) ?[*]u8 {
+            const alignment = @as(usize, 1) << @as(Allocator.Log2Align, @intCast(log2_ptr_align));
+
+            var unaligned_ptr = @as([*]u8, @ptrCast(self.unalignedAlloc(len + alignment - 1) orelse return null));
+            const unaligned_addr = @intFromPtr(unaligned_ptr);
+            const aligned_addr = mem.alignForward(usize, unaligned_addr, alignment);
+
+            return unaligned_ptr + (aligned_addr - unaligned_addr);
+        }
+
+        fn unalignedAlloc(self: *Self, len: usize) ?[*]u8 {
+            const offset = self.manager.alloc(len) orelse return null;
+            return @ptrFromInt(@intFromPtr(self.heap.ptr) + offset);
+        }
+
+        fn resize(ctx: *anyopaque, buf: []u8, log2_old_align: u8, new_len: usize, ret_addr: usize) bool {
+            _ = log2_old_align;
+            _ = ret_addr;
+            const self: *Self = @ptrCast(@alignCast(ctx));
+            const ok = new_len <= self.alignedAllocSize(buf.ptr);
+            if (config.verbose_log and ok) {
+                log.info("resize {d} bytes at {*} to {d}", .{
+                    buf.len, buf.ptr, new_len,
+                });
+            }
+            return ok;
+        }
+
+        fn alignedAllocSize(self: *Self, ptr: [*]u8) usize {
+            const aligned_offset = @intFromPtr(ptr) - @intFromPtr(self.heap.ptr);
+            const index = self.manager.backward(aligned_offset);
+
+            const unaligned_offset = self.manager.indexToOffset(index);
+            const unaligned_size = self.manager.indexToSize(index);
+
+            return unaligned_size - (aligned_offset - unaligned_offset);
+        }
+
+        fn free(ctx: *anyopaque, buf: []u8, log2_old_align: u8, ret_addr: usize) void {
+            _ = log2_old_align;
+            _ = ret_addr;
+            const self: *Self = @ptrCast(@alignCast(ctx));
+            if (config.verbose_log) {
+                log.info("free {d} bytes at {*}", .{ buf.len, buf.ptr });
+            }
+            self.alignedFree(buf.ptr);
+        }
+
+        fn alignedFree(self: *Self, ptr: [*]u8) void {
+            self.manager.free(@intFromPtr(ptr) - @intFromPtr(self.heap.ptr));
+        }
+    };
+}
 
 pub const Buddy2 = struct {
     const Self = @This();
@@ -265,8 +309,17 @@ test "Buddy2Allocator" {
         var heap: [3 * heap_size + 1024]u8 = undefined;
     };
 
-    var buddy2 = Buddy2Allocator.init(S.heap[0..]);
+    var buddy2 = Buddy2Allocator(.{}).init(S.heap[0..]);
+    defer testing.expect(buddy2.deinit() == .ok) catch @panic("leak");
     var allocator = buddy2.allocator();
+
+    const slice = try allocator.alloc(u8, 1);
+    try testing.expect(buddy2.detectLeaks());
+    allocator.free(slice);
+    try testing.expect(!buddy2.detectLeaks());
+
+    // TODO: detect double free
+    allocator.free(slice);
 
     try std.heap.testAllocator(allocator);
     try std.heap.testAllocatorAligned(allocator);
